aa457c02a684ef2ac1c9e8f173bc66c4ef6f3a0c
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2025-04-30 19:06:48 +0200

    [stdlib,switch] add some bindings for deko3d vertex buffers

--- examples/graphics/shaders/triangle_vert.glsl
+++ examples/graphics/shaders/triangle_vert.glsl
@@ -1,20 +1,9 @@
 #version 460
 
-const vec2 positions[3] = vec2[](
-    vec2(-0.5, -0.5),
-    vec2( 0.5, -0.5),
-    vec2( 0.0,  0.5)
-);
-
-const vec3 colors[3] = vec3[](
-    vec3(1.0, 0.0, 0.0),
-    vec3(0.0, 1.0, 0.0),
-    vec3(0.0, 0.0, 1.0)
-);
-
+layout (location = 0) in vec2 in_position;
 layout (location = 0) out vec4 out_color;
 
 void main() {
-    gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
-    out_color = vec4(colors[gl_VertexID], 1.0);
+    gl_Position = vec4(in_position, 0.0, 1.0);
+    out_color = vec4(1.0, 0.0, 0.5, 1.0);
 }
