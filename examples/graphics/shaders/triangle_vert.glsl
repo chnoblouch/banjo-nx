d7625b5f6eeeea011ceb5c0e0708e00889e7b55c
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2025-05-05 00:33:02 +0200

    [stdlib,switch] add some bindings for deko3d depth buffers

--- examples/graphics/shaders/triangle_vert.glsl
+++ examples/graphics/shaders/triangle_vert.glsl
@@ -1,13 +1,15 @@
 #version 460
 
-layout (location = 0) in vec2 in_position;
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec3 in_color;
+
 layout (location = 0) out vec4 out_color;
 
 layout (std140, binding = 0) uniform Uniforms {
-    vec2 translation;
+    mat4 matrix;
 } uniforms;
 
 void main() {
-    gl_Position = vec4(in_position + uniforms.translation, 0.0, 1.0);
-    out_color = vec4(1.0, 0.0, 0.5, 1.0);
+    gl_Position = uniforms.matrix * vec4(in_position, 1.0);
+    out_color = vec4(in_color, 1.0);
 }
